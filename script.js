b172e564412e594281b166c08311e1de62c7d828
ErastusNjaramba <erastus.kimani@student.moringaschool.com>
2021-05-04 22:43:43 +0300

    added alert for dailyproduction variables

--- script.js
+++ script.js
@@ -3,5 +3,18 @@ function shed (){
     var B= 308;
     var C= 486;
     var D= 572;
+
+
+  alert ("your production in shed A " +510+ " litres per day")
+  alert ("your production in shed B " +308+ " litres per day")
+  alert ("your production in shed C " +486+ " litres per day")    
+  alert ("your production in shed D " +572+ " litres per day")  
+  var res =A+B+C+D
+  alert ("Your total milk production per day "  +res+ " litres");
+
   }
+
+  dailyProduction()
+
 }
+shed()
