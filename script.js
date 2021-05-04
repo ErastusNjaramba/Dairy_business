feca9e8ff23d18604f890ac673c30428a67320a3
ErastusNjaramba <erastus.kimani@student.moringaschool.com>
2021-05-04 23:11:24 +0300

    added a month production function

--- script.js
+++ script.js
@@ -15,6 +15,43 @@ function shed (){
   function productionWeekly(){
   var weekly=(res*7*45)
   alert ("Your total earning per week"+weekly+"  KSH");
+
+  function monthlyProduction(){
+  
+    var jan = (res*31*45)
+    var feb = (res*29*45)
+    var mar = (res*31*45)
+    var apr = (res*30*45)
+    var may = (res*31*45)
+    var jun = (res*30*45)
+    var jul = (res*31*45)
+    var aug = (res*31*45)
+    var sep = (res*30*45)
+    var oct = (res*31*45)
+    var nov = (res*30*45)
+    var dec = (res*31*45)
+  alert ("your total income for January  " +jan+ " litres per day")
+  alert ("your total income for February  " +feb+ " litres per day")
+  alert ("your total income for March  " +mar+ " litres per day")
+  alert ("your total income for April  " +apr+ " litres per day")
+  alert ("your total income for May  " +may+ " litres per day")
+  alert ("your total income for June  " +jun+ " litres per day")
+  alert ("your total income for July  " +jul+ " litres per day")
+  alert ("your total income for August  " +aug+ " litres per day")
+  alert ("your total income for September  " +sep+ " litres per day")
+  alert ("your total income for October  " +oct+ " litres per day")
+  alert ("your total income for November  " +nov+ " litres per day")
+  alert ("your total income for December  " + dec+ " litres per day")
+  
+  
+        var yearly=jan+feb+mar+apr+may+jun+jul+aug+sep+oct+nov+dec
+        alert ("Your total earning per year "+yearly+"  KSH");
+  
+      
+  }
+  monthlyProduction()
+
+  
   
       
      }
